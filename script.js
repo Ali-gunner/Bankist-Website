f98bf0f5ca0879df857e27af36ef710e8fbe7c70
Ali-gunner <ali.ranjbar.lut2024@gmail.com>
2025-05-15 23:03:53 +0300

    Added page navigation

--- script.js
+++ script.js
@@ -46,3 +46,14 @@ btnScrollTo.addEventListener("click", function (e) {
 
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+// Page Navigation
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
