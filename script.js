78746293bcbdaee7d6fd095e65ed82d527f8870f
Ali-gunner <ali.ranjbar.lut2024@gmail.com>
2025-05-15 22:48:12 +0300

    Added scrolling event

--- script.js
+++ script.js
@@ -34,3 +34,15 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+// Button scrolling
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
